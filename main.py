b1871ff7f80e999acf758990b73cbb63be1fdd75
Jiamin Zhu <github@jiamin.io>
2022-01-27 19:20:38 +0100

    flex query with fastapi

--- main.py
+++ main.py
@@ -1,29 +1,21 @@
-from ib_insync import IB
-
-from portfolio import Portfolio
-
-
-def main():
-    ib = IB()
-    ib.connect(
-        host="127.0.0.1",
-        port=7496,
-        clientId=0,
-    )
-    portfolio_items = ib.portfolio()
-    portfolio = Portfolio(portfolio_items=portfolio_items, ib=ib)
-    portfolio.request_contract_details()
-    portfolio.generate_combos()
-    portfolio.request_combo_margin()
-    total_maintenance_margin = 0
-    total_initial_margin = 0
-    for combo in portfolio.combos:
-        total_maintenance_margin += combo.maintenance_margin
-        total_initial_margin += combo.initial_margin
-    print(total_maintenance_margin)
-    print(total_initial_margin)
-    ib.disconnect()
+from flex import flex
+from typing import Optional
+from fastapi import FastAPI
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.get("/report/trade_confirmation")
+def trade_confirmation():
+    return flex()
