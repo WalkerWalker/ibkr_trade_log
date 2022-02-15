705b3e8e51ab54125a50e3421d67785fe78f5c72
Jiamin Zhu <github@jiamin.io>
2022-02-15 21:20:25 +0100

    api plugin

---
+++ api/plugin.py
@@ -0,0 +1,29 @@
+import uvicorn
+from starlette.requests import Request
+from starlette.responses import RedirectResponse
+
+from flex.flex import flex
+from fastapi import FastAPI
+
+from messagebus.bus import MessageBus
+
+
+class ApiPlugin:
+    def __init__(self, messagebus: MessageBus):
+        self.api = FastAPI()
+        self.messagebus = messagebus
+        self._load_api()
+
+    def _load_api(self):
+        @self.api.get("/", include_in_schema=False)
+        async def root(request: Request):  # pragma: no cover
+            return RedirectResponse(
+                request.scope.get("root_path").rstrip("/") + "/docs"
+            )
+
+        @self.api.get("/flex_report/load")
+        def load(filename):
+            return flex()
+
+    def startup(self):
+        uvicorn.run(self.api)
